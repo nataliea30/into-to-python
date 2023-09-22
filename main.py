eae73a3049b0701f6fd6a288c1f7c865b8eb3207
Natalie Anwar <natal@natalies-mbp.attlocal.net>
2023-09-21 21:39:10 -0400

    going over print statements and taking user input

---
+++ main.py
@@ -0,0 +1,18 @@
+#Intro to Python - Print Statements & Taking User Input
+
+#This is a comment. Comments are used to explain code. They are not executed by the computer.
+
+
+#print statements -> print() is the syntax. Print statements are used to display text on the screen.
+print("Hi! What's your name?") #The text always has to be in quotes.
+
+
+#taking user input -> input() is the syntax. Input statements are used to take input from the user.
+name = input() #'name' is a variable. Variables are used to store data. In this case, we are storing the user's name.
+
+#concatenation -> '+' is the syntax. Concatenation is used to combine two strings. Strings are text.
+print("Nice to meet you, " + name + "!")
+
+#Keep in mind, lines run in order. The computer will run the first line, then the second, then the third, etc.
+#Exercise: Write a program that takes the user's name and age as input and displays it on the screen.
+#Hint: You will need to use two input statements and two print statements.
